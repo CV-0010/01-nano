5f8a0a12ed15d7eb2e46dde55693627e9ddc4cbb
01-nano <105368142+01-nano@users.noreply.github.com>
2022-05-11 11:45:26 -0400

    Delete newProjectGit directory

--- newProjectGit/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!\n");
-        System.out.println("This is my first Java program uploaded to Git :D");
-    }
-}
